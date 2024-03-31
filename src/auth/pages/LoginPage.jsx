0f637d3dfdc65426f3e35a5b141999a6d1f7a310
David Promontor Farias <davidpromontorfarias@gmail.com>
2024-03-30 23:34:57 -0600

    Se implementaron las validaciones

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,28 +1,45 @@
+import { useForm } from "../../hook/useForm"
+
+
+
 
 export const LoginPage = () => {
+
+  const {onInputChange, validateForm, alertEmail, alertEmpty, nombre, apellidos, email, password} = useForm({
+    nombre: "",
+    apellidos: "",
+    email: "",
+    password: ""
+  })
   return (
     <div className="contain-form-global">
         <div className="contain-form">
           <h1 className="title-logo">Dolce Amore Frappe</h1>
           <form className="form-login" action="">
+            {alertEmpty && <p className="alerts">Los Campos No Pueden Estar Vacios</p> }
+            {alertEmail && <p className="alerts">El Email No Es Valido</p>}
             <div className="fields">
-              <label htmlFor="">Nombre: </label>
-              <input type="text" placeholder="Nombre" />
+              <label htmlFor="nombre">Nombre: </label>
+              <input id="nombre" value={nombre} onChange={e => onInputChange(e) } name="nombre" type="text" placeholder="Nombre" />
             </div>
             <div className="fields">
-              <label htmlFor="">Apellidos: </label>
-              <input type="text" placeholder="Apellidos" />
+              <label htmlFor="apellidos">Apellidos: </label>
+              <input id="apellidos" value={apellidos} onChange={e => onInputChange(e) } name="apellidos"  type="text" placeholder="Apellidos" />
             </div>
             <div className="fields">
-              <label htmlFor="">Email: </label>
-              <input type="email" placeholder="Email" />
+              <label htmlFor="email">Email: </label>
+              <input id="email" value={email} onChange={e => onInputChange(e) } name="email"  type="email" placeholder="Email" />
             </div>
             <div className="fields">
-              <label htmlFor="">Password: </label>
-              <input type="password" placeholder="Contraseña" />
+              <label htmlFor="password">Password: </label>
+              <input id="password" value={password} onChange={e => onInputChange(e) } name="password"  type="password" placeholder="Contraseña" />
             </div>
 
-            <input  type="submit" value="Registrar" />
+            <button
+              onClick={(e) => validateForm(e) }
+              className="btn-registrar">
+              Registrarse
+            </button>
           </form>
         </div>
     </div>
---
+++ src/hook/useForm.js
@@ -0,0 +1,69 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+
+
+
+export  const useForm = (initialForm = {}) => {
+
+    
+
+    const [formState, setFormState] = useState(initialForm);
+    const [alertEmpty, setAlertEmpty] = useState(false);
+    const [alertEmail, setAlertEmail] = useState(false);
+
+    const navigate = useNavigate();
+
+    const onInputChange = ({target}) => {
+
+        const {name, value} = target;
+
+        setFormState({
+            ...formState,
+            [name] : value
+        })
+
+    }
+
+    const validateForm = (e) => {
+        e.preventDefault();
+
+
+        const {nombre, apellidos, email, password} =  formState;
+        const evaluation =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+        if(nombre.length === 0 || apellidos.length === 0 || email.length === 0 || password.length === 0){
+            setAlertEmpty(true);
+
+            setTimeout(() => {
+                setAlertEmpty(false);
+            }, 3000);
+
+            return;
+        }
+        else if(!evaluation.test(email)){
+            setAlertEmail(true);
+
+            setTimeout(() => {
+                setAlertEmail(false);
+            }, 3000);
+        }
+
+        navigate('/inicio', {
+            replace: true
+        })
+
+
+
+    }
+
+    return {
+
+        ...formState,
+        onInputChange,
+        validateForm,
+        alertEmpty,
+        alertEmail
+
+    }
+
+}
